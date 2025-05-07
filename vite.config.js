dfa00e40b5dcc118145f266d5600059eb2c2d3bd
Vighnesh-b <viggie198@gmail.com>
2025-05-07 23:44:49 +0530

    url change made

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/SChat-Frontend/',
-  plugins: [react(),tailwindcss()],
-  
+  base: process.env.NODE_ENV === 'production' ? '/SChat-Frontend/' : '/',
+  plugins: [react(), tailwindcss()],
 })
